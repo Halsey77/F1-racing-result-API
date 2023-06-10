b6b1992c8fda51a8207b936939f9dbb196306c3a
Halsey77 <provcc143300@gmail.com>
2023-06-10 20:26:02 +0700

    Add Race schema and model

--- src/database/models/races.model.ts
+++ src/database/models/races.model.ts
@@ -1,6 +1,18 @@
-export interface race {
-    "grandpix": string,
-    "year": number,
-    "date": string,
-    "place": string,
+import mongoose, {Model, model, Schema} from "mongoose";
+
+export interface Race {
+    grandpix: string,
+    date: string,
+    place: string,
 }
+
+const racesSchema: Schema<Race> = new mongoose.Schema({
+    grandpix: { type: String, required: true },
+    date: { type: String, required: true },
+    place: String,
+}, {
+    collection: "races"
+});
+
+const RaceModel: Model<Race> = model<Race>('Race', racesSchema);
+export default RaceModel;
