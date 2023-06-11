38fa468e643accb1649e3c099259a50247417037
Halsey77 <provcc143300@gmail.com>
2023-06-11 21:39:15 +0700

    add default sort

--- src/components/v1/driver-standing-all/driverStandingAll.service.ts
+++ src/components/v1/driver-standing-all/driverStandingAll.service.ts
@@ -3,5 +3,7 @@ import {driverStandingsAllModel} from "../../../database/models/driverStandingsA
 export async function getDriverStandingsOfYear(year: number) {
     return await driverStandingsAllModel.find({
         year: year
+    }).sort({
+        pos: 1
     }).lean().exec();
 }
