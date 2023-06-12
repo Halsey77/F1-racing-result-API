f480cf1b275229999f890758557e07258d3e435e
Halsey77 <provcc143300@gmail.com>
2023-06-12 21:08:29 +0700

    minor refactor for races

--- src/components/v1/races/races.controller.ts
+++ src/components/v1/races/races.controller.ts
@@ -1,9 +1,13 @@
 import * as racesService from './races.service';
-import {HttpCodes} from "../../../errors/api";
+import {APIError, HttpCodes} from "../../../errors/api";
 
 export async function getAllRaces(req, res) {
-    const { year, grandpix, place } = req.query;
+    const { year, grandprix, place } = req.query;
 
-    const result = await racesService.getAllRaces(year, grandpix, place);
+    if (year && isNaN(year)) {
+        throw new APIError(HttpCodes.BAD_REQUEST, `Invalid year value ${year}`);
+    }
+
+    const result = await racesService.getAllRaces(year, grandprix, place);
     res.status(HttpCodes.OK).send(result);
 }
--- src/components/v1/races/races.service.ts
+++ src/components/v1/races/races.service.ts
@@ -16,14 +16,14 @@ export async function findOneRace(year: number, grandprix: string): Promise<Race
 }
 
 
-export async function getAllRaces(year?: number, grandpix?: string, place?: string) {
+export async function getAllRaces(year?: number, grandprix?: string, place?: string) {
     const filter = removeUndefinedKeysFromFilter({
         date: !year ? undefined : {
             $gte: new Date(year, 0, 1),
             $lte: new Date(year, 11, 31)
         },
-        grandpix: !grandpix ? undefined : {
-            $regex: new RegExp(grandpix, 'i')
+        grandpix: !grandprix ? undefined : {
+            $regex: new RegExp(grandprix, 'i')
         },
         place: !place ? undefined : {
             $regex: new RegExp(place, 'i')
