09d74f6962b6a02d27700295c7143b5a88716a2a
Halsey77 <provcc143300@gmail.com>
2023-06-14 16:02:47 +0700

    apply validators for races requests

--- src/components/v1/races/races.controller.ts
+++ src/components/v1/races/races.controller.ts
@@ -1,13 +1,22 @@
 import * as racesService from './races.service';
 import {APIError, HttpCodes} from "../../../errors/api";
+import {FieldValidationError, matchedData, query, validationResult} from "express-validator";
+
+export const raceQueryValidator = [
+    query('year').optional().isInt({min: 1990}).withMessage('Year must be a number greater than 1990'),
+    query('grandprix').optional().isString().withMessage('Grandprix must be a string'),
+    query('place').optional().isString().withMessage('Place must be a string'),
+];
 
 export async function getAllRaces(req, res) {
-    const { year, grandprix, place } = req.query;
-
-    if (year && isNaN(year)) {
-        throw new APIError(HttpCodes.BAD_REQUEST, `Invalid year value ${year}`);
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        const error = errors.array({ onlyFirstError: true })[0] as FieldValidationError;
+        const message = `Validation error for ${error.path}: ${error.msg}`;
+        throw new APIError(HttpCodes.BAD_REQUEST, message);
     }
 
+    const { year, grandprix, place } = matchedData(req, { includeOptionals: true });
     const result = await racesService.getAllRaces(year, grandprix, place);
     res.status(HttpCodes.OK).send(result);
 }
